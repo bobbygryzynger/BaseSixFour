86b40cd0b89656d241a19975b68f9facd9df2a22
Bobby Gryzynger <bobby.gryzynger@gmail.com>
2016-01-14 18:34:38 -0500

    initial encoding loop

--- src/BaseSixFour.cpp
+++ src/BaseSixFour.cpp
@@ -9,8 +9,24 @@ std::string BaseSixFour::encode(std::vector<uint8_t> in){
     using namespace std;
     string ret = "";
 
-    for(size_t i = 0; i < in.size(); i++){
-        std::cout << (int)in.at(i);
+    for(size_t i = 0; i < 4; i++){
+
+        uint index = 0;
+
+        if(i == 0){
+            index =  (in.at(i) >> 2) & 0x3f;
+
+            std::cout << (char)in.at(i) << ": " << index << std::endl;
+        }else if(i == 1){
+            index = ((in.at(i-1) & 0x03) << 4) + (in.at(i) >> 4);
+            std::cout << (char)in.at(i) << ": " << index << std::endl;
+        }else if(i == 2){
+            index = ((in.at(i-1) << 2) + (in.at(i) >> 6)) & 0x3f;
+            std::cout << (char)in.at(i) << ": " << index << std::endl;
+        }else{
+            index = in.at(i) & 0x3f;
+            std::cout << (char)in.at(i-1) << ": " << index << std::endl;
+        }
     }
 
     std::cout << std::endl;
