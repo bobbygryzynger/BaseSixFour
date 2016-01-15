b13488e1381237c145dc1132744f871f8e726ee7
Bobby Gryzynger <bobby.gryzynger@gmail.com>
2016-01-15 15:34:13 -0500

    updated line endings

--- tests/BaseSixFourTests.cpp
+++ tests/BaseSixFourTests.cpp
@@ -24,15 +24,15 @@ class BaseSixFourTests : public ::testing::Test {
 TEST_F(BaseSixFourTests, TestEncode){
 
     //from: https://en.wikipedia.org/wiki/Base64#Examples
-    std::string in =    "Man is distinguished, not only by his reason, but by this singular passion from\n"
-                        "other animals, which is a lust of the mind, that by a perseverance of delight\n"
-                        "in the continued and indefatigable generation of knowledge, exceeds the short\n"
+    std::string in =    "Man is distinguished, not only by his reason, but by this singular passion from\r\n"
+                        "other animals, which is a lust of the mind, that by a perseverance of delight\r\n"
+                        "in the continued and indefatigable generation of knowledge, exceeds the short\r\n"
                         "vehemence of any carnal pleasure.";
 
-    std::string out =   "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz\n"
-                        "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg\n"
-                        "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu\n"
-                        "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo\n"
+    std::string out =   "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz\r\n"
+                        "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg\r\n"
+                        "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu'\r\n"
+                        "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo'\r\n"
                         "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
 
     std::vector<uint8_t> inVec;
