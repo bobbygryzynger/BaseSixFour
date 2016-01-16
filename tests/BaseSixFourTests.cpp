8f9e246b1aa3c153fe3deef6cf09a8627e6cbe1b
Bobby Gryzynger <bobby.gryzynger@gmail.com>
2016-01-16 17:18:36 -0500

    reformatting

--- tests/BaseSixFourTests.cpp
+++ tests/BaseSixFourTests.cpp
@@ -22,16 +22,18 @@ class BaseSixFourTests : public ::testing::Test {
 TEST_F(BaseSixFourTests, EncodeText){
 
     //from: https://en.wikipedia.org/wiki/Base64#Examples
-    std::string in =    "Man is distinguished, not only by his reason, but by this singular passion from "
-                        "other animals, which is a lust of the mind, that by a perseverance of delight "
-                        "in the continued and indefatigable generation of knowledge, exceeds the short "
-                        "vehemence of any carnal pleasure.";
+    std::string in =
+        "Man is distinguished, not only by his reason, but by this singular passion from "
+        "other animals, which is a lust of the mind, that by a perseverance of delight "
+        "in the continued and indefatigable generation of knowledge, exceeds the short "
+        "vehemence of any carnal pleasure.";
 
-    std::string out =   "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz\r\n"
-                        "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg\r\n"
-                        "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu\r\n"
-                        "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo\r\n"
-                        "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+    std::string out =
+        "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz\r\n"
+        "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg\r\n"
+        "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu\r\n"
+        "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo\r\n"
+        "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
 
 
     std::vector<uint8_t> inVec;
