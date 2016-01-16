777a21f9c2be669d57e41d31ec2fd055d78e09d6
Bobby Gryzynger <bobby.gryzynger@gmail.com>
2016-01-16 17:15:53 -0500

    added test and image for file encoding test

--- tests/BaseSixFourTests.cpp
+++ tests/BaseSixFourTests.cpp
@@ -1,8 +1,6 @@
 #include "gtest/gtest.h"
 #include "include/BaseSixFour.h"
-
-using namespace std;
-
+#include <fstream>
 
 class BaseSixFourTests : public ::testing::Test {
 
@@ -42,4 +40,48 @@ TEST_F(BaseSixFourTests, EncodeText){
     }
 
     ASSERT_STREQ(out.c_str(), BaseSixFour::encode(inVec).c_str());
+}
+
+TEST_F(BaseSixFourTests, EncodeFile){
+
+    std::string encoded;
+    std::string expected =
+        "/9j/4AAQSkZJRgABAQAASABIAAD/4QBYRXhpZgAATU0AKgAAAAgAAgESAAMAAAABAAEAAIdpAAQA\r\n"
+        "AAABAAAAJgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAEKADAAQAAAABAAAAEAAAAAD/7QA4\r\n"
+        "UGhvdG9zaG9wIDMuMAA4QklNBAQAAAAAAAA4QklNBCUAAAAAABDUHYzZjwCyBOmACZjs+EJ+/8AA\r\n"
+        "EQgAEAAQAwERAAIRAQMRAf/EAB8AAAEFAQEBAQEBAAAAAAAAAAABAgMEBQYHCAkKC//EALUQAAIB\r\n"
+        "AwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBka\r\n"
+        "JSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SV\r\n"
+        "lpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX2\r\n"
+        "9/j5+v/EAB8BAAMBAQEBAQEBAQEAAAAAAAABAgMEBQYHCAkKC//EALURAAIBAgQEAwQHBQQEAAEC\r\n"
+        "dwABAgMRBAUhMQYSQVEHYXETIjKBCBRCkaGxwQkjM1LwFWJy0QoWJDThJfEXGBkaJicoKSo1Njc4\r\n"
+        "OTpDREVGR0hJSlNUVVZXWFlaY2RlZmdoaWpzdHV2d3h5eoKDhIWGh4iJipKTlJWWl5iZmqKjpKWm\r\n"
+        "p6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uLj5OXm5+jp6vLz9PX29/j5+v/bAEMAAQEB\r\n"
+        "AQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEB\r\n"
+        "AQEBAf/bAEMBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEB\r\n"
+        "AQEBAQEBAQEBAQEBAQEBAf/dAAQAAv/aAAwDAQACEQMRAD8A/mW+AX7OvwZn+DvhnUNT8KaB4v1P\r\n"
+        "xx4S0+/1/XNTiN/dpPqttdXM1lpE73Vz/wAI1eaINTm0WS98OSaPqMlxpdvd3jpqVrE8H8M+I/id\r\n"
+        "x1T44zXDYTOMyyXCZBnWJw+W5fhJ/VqEqeEq0aUMRjacaVNZrRzD6rDHxoZpHGYWNLF1KNGLwlac\r\n"
+        "J/8AVP8AQu+gx9FXGfRa8P8AO+IfDfgzxO4g8XvDPJM5414vz/DyznM6WL4iwOY4/FZTwzjKuY49\r\n"
+        "8A5nwks/xPClbNeBKvC+e1sbw/gszzSvHiDLsPVwH5Z/GLwdo/w++J3jPwb4f1hdc0fQdYktLK+D\r\n"
+        "xSSxq8MNxLpd5JAzRS6joc88uianIiweZqOn3Tm1s2LWkH9ecD55juJOE8izzMsE8vxuY4KNavh+\r\n"
+        "WcYSaqVKUMXQjU9+GFzCnThj8JCUqnLhsVSiq1ZJVp/86v0pPCzhbwT+kH4q+FfBXE8OL+GODOKK\r\n"
+        "2WZVnCq4aviKNOphMJjsRw9mdfCVJ4evnvCGMxeJ4S4gr04YRV88yTMKssuyuc5Zbhf/0P4VfCPx\r\n"
+        "x+LXgTQb7wx4U8d65pGg6hbS2h05JYbqLTop2vpJ38PyX0F1P4YuZ59Su7qa98OzaVeS3jx3sk7X\r\n"
+        "dtazwfH514f8GcQ5jh82zjh7L8bmOHqwrLFShUozxM6aw8aazKOHqUaebUqdPC0KVOhmkcZQhQjP\r\n"
+        "DxpKjVqwn/R/hn9L36S/g7wZnHh74b+MfF/DPBudYDE5bLIqWIweY4fI8PjZ5xWxlTgqtnGEzHGe\r\n"
+        "H2YYvF57meY4rNeBcTw5mmIzWrQzatjJ5lgcvxeH8rd3kd5JHaSR2Z3d2LO7sSWd2YlmZiSWYkkk\r\n"
+        "kknOa+vjGMYqMUoxilGMYpKMYpWSSVkklZJJWS0Vj+dKtWrXq1K9epUrVq1SdWtWqzlUq1atSTnU\r\n"
+        "qVKkm5TqTm3Kc5NylJtttts//9k=";
+
+    //read image from file
+    std::ifstream ifs("../BaseSixFour/tests/data/icon.jpg", std::ios::binary);
+    ifs.seekg(0, std::ios::end);
+    std::vector<char> charVec(ifs.tellg());
+    ifs.seekg(0, std::ios::beg);
+    ifs.read(&charVec[0], charVec.size());
+
+    encoded =  BaseSixFour::encode(std::vector<uint8_t>(charVec.begin(), charVec.end()));
+
+    ASSERT_STREQ(expected.c_str(), encoded.c_str());
 }
