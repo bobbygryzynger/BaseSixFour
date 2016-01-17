2d9da675c5213423ce072ae2da916bfa5a12b6aa
Bobby Gryzynger <bobby.gryzynger@gmail.com>
2016-01-17 17:18:37 -0500

    documentation update

--- src/BaseSixFour.cpp
+++ src/BaseSixFour.cpp
@@ -11,46 +11,71 @@ std::string BaseSixFour::encode(const std::vector<uint8_t> &in){
 
     using namespace std;
 
+    // the Base64-encoded character string to return
     string ret = "";
-    string encodedChars = "";
+
+    // the total size of the Base64 character string
     size_t totEncodedChars = 0;
+
+    // the container for the three octets to encode
     uint8_t* inPtr = new uint8_t[ENCODE_OCTETS];
+
+    // the current set of three octets
+    // encoded as four Base64 characters
+    string encodedChars = "";
 
     for(size_t i = 0; i < in.size(); i+=ENCODE_OCTETS){
 
+        // the end of the input data has not been reached
         if(i+ENCODE_OCTETS-1 < in.size()){
             inPtr[0] = in.data()[i];
             inPtr[1] = in.data()[i+1];
             inPtr[2] = in.data()[i+2];
             encodedChars = encodeOctets(inPtr);
-        }else if(i+ENCODE_OCTETS-2 < in.size()){
+        }
+        // there are only two remaining octets
+        else if(i+ENCODE_OCTETS-2 < in.size()){
             inPtr[0] = in.data()[i];
             inPtr[1] = in.data()[i+1];
             inPtr[2] = 0;
+            // there will only be three significant encoded charaters
+            // add a padding character to ensure length = 4
             encodedChars = encodeOctets(inPtr).substr(0, 3) + PAD_CHAR;
-        }else{
+        }
+        // there is only one remaining octet
+        else{
             inPtr[0] = in.data()[i];
             inPtr[1] = 0;
             inPtr[2] = 0;
+            // there will only be two significant encoded charaters
+            // add two padding characters to ensure length = 4
             encodedChars = encodeOctets(inPtr).substr(0, 2) + PAD_CHAR + PAD_CHAR;
         }
 
-        //the next encoded sequence will exceed the line length
+        // appending the next encoded sequence will exceed the line length
         if((totEncodedChars + ENCODED_SIZE) / LINE_LEN > totEncodedChars / LINE_LEN){
+            // look for where the line should end
             for(unsigned int j = 0; j < ENCODED_SIZE; j++){
+                //  append single character
                 ret += encodedChars.at(j);
-                //the end of the line has been reached
+                // the end of the line has been reached
                 if((totEncodedChars + j+1) % LINE_LEN == 0){
+                    // add the line terminating characters
                     ret += LINE_TERM;
                 }
             }
-        }else{
+        }
+        // the maximum line length will not be exceed by the current characters
+        // append all of the encoded charaters
+        else{
             ret += encodedChars;
         }
 
+        // update count
         totEncodedChars += ENCODED_SIZE;
     }
 
+    //clean up
     inPtr = NULL;
     delete inPtr;
 
